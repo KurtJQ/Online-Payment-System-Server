fb0f5688d0b1e97e95318a18c1a7746602c0c760
KurtJQ <irockstar12230@gmail.com>
2025-04-26 00:28:05 +0800

    webhook setup

--- routes/webhook.js
+++ routes/webhook.js
@@ -4,17 +4,27 @@ import db from "../db/connection.js";
 const router = express.Router();
 
 router.post("/payment/success", async (req, res) => {
-  // const data = req.body?.data?.attributes?.data
-  // try {
-  // const newPayment = {
-  //   paymentId: data?.id,
-  //   amount: data?.attrbiutes.amount / 100,
-  // };
-  // const collection = db.collection("payments");
-  // } catch (error) {}
+  const data = req.body?.data?.attributes?.data;
+  try {
+    const newPayment = {
+      paymentId: data?.id,
+      amount: data?.attrbiutes.amount / 100,
+      referenceNumber: data?.reference_number,
+      description: data?.description,
+      billingDetails: {
+        name: data?.billing.name,
+        email: data?.billing.email,
+        phone: data?.billing.phone,
+      },
+      createdAt: new Date(req.body?.data?.created_at).toISOString(),
+    };
+    const collection = db.collection("payments");
+    const result = await collection.insertOne(newPayment);
 
-  console.log(req.body);
-  res.send(200);
+    res.send(result).status(201);
+  } catch (error) {
+    res.status(400).send("ERROR: ", error);
+  }
 });
 
 export default router;
