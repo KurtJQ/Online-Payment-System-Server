8aadb6fa38455fbbd838173c9498b6d83b8268e2
KurtJQ <irockstar12230@gmail.com>
2025-04-30 00:10:36 +0800

    webhook setup

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,27 +3,44 @@ import db from "../db/connection.js";
 
 const router = express.Router();
 
-router.post("/payment/success", async (req, res) => {
+router.post("/payment/new", async (req, res) => {
   const data = req.body?.data?.attributes?.data;
   try {
+    const studentCollection = db.collection("students");
+    const studentSearch = await studentCollection.findOne({
+      _studentId: data.attributes.metadata.studentId,
+    });
+
     const newPayment = {
       paymentId: data?.id,
-      amount: data?.attrbiutes.amount / 100,
-      referenceNumber: data?.reference_number,
-      description: data?.description,
+      amount: data?.attributes.line_items[0].amount / 100,
+      referenceNumber: data?.attributes.reference_number,
+      description: data?.attributes.line_items[0].description,
       billingDetails: {
-        name: data?.billing.name,
-        email: data?.billing.email,
-        phone: data?.billing.phone,
+        name: data?.attributes.billing.name,
+        email: data?.attributes.billing.email,
+        phone: data?.attributes.billing.phone,
       },
-      createdAt: new Date(req.body?.data?.created_at).toISOString(),
+      studentRef: studentSearch._id,
+      studentId: data?.attributes.metadata.studentId,
+      fname: studentSearch.fname,
+      mname: studentSearch.mname,
+      lname: studentSearch.lname,
+      course: data?.attributes.metadata.course,
+      semeseter: data?.attributes.metadata.semester,
+      education: data?.attributes.metadata.education,
+      yearLevel: data?.attributes.metadata.yearLevel,
+      schoolYear: data?.attributes.metadata.schoolYear,
+      examPeriod: data?.attributes.line_items[0].name,
+      status: data?.attributes.payments[0].attributes.status,
+      createdAt: new Date(data.attributes.created_at).toISOString(),
     };
-    const collection = db.collection("payments");
-    const result = await collection.insertOne(newPayment);
+    // const paymentCollection = db.collection("payments");
+    // const result = await collection.insertOne(newPayment);
 
-    res.send(result).status(201);
+    res.status(201).send({ newPayment });
   } catch (error) {
-    res.status(400).send("ERROR: ", error);
+    res.status(400).send("ERROR: " + error);
   }
 });
 
